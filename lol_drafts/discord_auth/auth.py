ed06252f3d5ed86f6d156db0098334c5e4a73a06
mysh3k <mateusz.r0@gmail.com>
2023-09-26 15:24:48 +0200

    deleting unnecessary print

--- lol_drafts/discord_auth/auth.py
+++ lol_drafts/discord_auth/auth.py
@@ -4,7 +4,6 @@ from .models import DiscordUser
 class DiscordAuthenticationBackend(BaseBackend):
     def authenticate(self, request, user) -> DiscordUser:
         discord_user, created = DiscordUser.objects.get_or_create(id=user['id'])
-        print(created)
         if created:
             discord_user = DiscordUser.objects.create_new_discord_user(discord_user, user)
         return discord_user
